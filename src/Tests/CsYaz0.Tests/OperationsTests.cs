45df7a8f65118388dad34d645b183147cc4a241f
ArchLeaders <archleadership28@gmail.com>
2024-01-14 07:43:10 -0800

    Add unit tests

---
+++ src/Tests/CsYaz0.Tests/OperationsTests.cs
@@ -0,0 +1,24 @@
+﻿namespace CsYaz0.Tests;
+
+public class OperationsTests
+{
+    [Fact]
+    public void VerifyCompression()
+    {
+        Yaz0.Compress(Assets.Uncompressed)
+            .AsSpan()
+            .SequenceEqual(Assets.Compressed)
+            .Should()
+            .BeTrue();
+    }
+
+    [Fact]
+    public void VerifyDecompression()
+    {
+        Yaz0.Decompress(Assets.Compressed)
+            .AsSpan()
+            .SequenceEqual(Assets.Uncompressed)
+            .Should()
+            .BeTrue();
+    }
+}
