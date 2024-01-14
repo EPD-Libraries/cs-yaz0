a545166610ac809698d2dc211d62b8aca582019c
ArchLeaders <archleadership28@gmail.com>
2024-01-14 15:01:16 -0800

    Update native library loading

--- src/CsYaz0/Unmanaged.cs
+++ src/CsYaz0/Unmanaged.cs
@@ -5,7 +5,7 @@ namespace CsYaz0;
 
 internal unsafe static partial class Unmanaged
 {
-    private const string LibImport = "Lib/Yaz0";
+    private const string LibImport = "Yaz0";
 
     [LibraryImport(LibImport)]
     public static partial DataMarshal Compress(byte* src, int src_len, uint alignment, int level);
--- src/Tests/CsYaz0.Tests/OperationsTests.cs
+++ src/Tests/CsYaz0.Tests/OperationsTests.cs
@@ -1,10 +1,19 @@
-﻿namespace CsYaz0.Tests;
+﻿using System.Runtime.InteropServices;
+
+namespace CsYaz0.Tests;
 
 public class OperationsTests
 {
     [Fact]
     public void VerifyCompression()
     {
+        // For some reason the resolver breaks
+        // when running tests, so we load from
+        // the folder explicitly.
+        NativeLibrary.Load(
+            Path.Combine(AppContext.BaseDirectory, "runtimes", GetRuntime(), "native", GetLibrary())
+        );
+
         Yaz0.Compress(Assets.Uncompressed)
             .AsSpan()
             .SequenceEqual(Assets.Compressed)
@@ -20,5 +29,43 @@ public class OperationsTests
             .SequenceEqual(Assets.Uncompressed)
             .Should()
             .BeTrue();
+    }
+
+    private static string GetRuntime()
+    {
+        if (OperatingSystem.IsWindows()) {
+            return "win-x64";
+        }
+
+        if (OperatingSystem.IsLinux()) {
+            return "linux-x64";
+        }
+
+        if (OperatingSystem.IsMacOS()) {
+            return "osx";
+        }
+
+        throw new NotSupportedException($"""
+            The current operating system is not supported.
+            """);
+    }
+
+    private static string GetLibrary()
+    {
+        if (OperatingSystem.IsWindows()) {
+            return "Yaz0.dll";
+        }
+
+        if (OperatingSystem.IsLinux()) {
+            return "Yaz0.so";
+        }
+
+        if (OperatingSystem.IsMacOS()) {
+            return "Yaz0.dylib";
+        }
+
+        throw new NotSupportedException($"""
+            The current operating system is not supported.
+            """);
     }
 }
