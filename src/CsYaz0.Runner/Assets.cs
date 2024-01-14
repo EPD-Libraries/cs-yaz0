599beed0781b8140ed66a1234754ecfa5d55eecc
ArchLeaders <archleadership28@gmail.com>
2024-01-14 07:41:40 -0800

    Create C# library

---
+++ src/CsYaz0.Runner/Assets.cs
@@ -0,0 +1,7 @@
+﻿namespace CsYaz0.Runner;
+
+public class Assets
+{
+    public static readonly byte[] Uncompressed = File.ReadAllBytes($"{AppContext.BaseDirectory}/Assets/Uncompressed.bnchmrk");
+    public static readonly byte[] Compressed = File.ReadAllBytes($"{AppContext.BaseDirectory}/Assets/Compressed.bnchmrk");
+}
---
+++ src/Tests/CsYaz0.Tests/Assets.cs
@@ -0,0 +1,7 @@
+﻿namespace CsYaz0.Tests;
+
+public class Assets
+{
+    public static readonly byte[] Uncompressed = File.ReadAllBytes($"{AppContext.BaseDirectory}/Assets/Uncompressed.test");
+    public static readonly byte[] Compressed = File.ReadAllBytes($"{AppContext.BaseDirectory}/Assets/Compressed.test");
+}
