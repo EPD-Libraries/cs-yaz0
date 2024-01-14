e2e4792085f12be064e9f7026bce3a682852afdb
ArchLeaders <archleadership28@gmail.com>
2024-01-14 07:42:24 -0800

    Create unmanaged imports (static)

---
+++ src/CsYaz0/Unmanaged.cs
@@ -0,0 +1,18 @@
+﻿using CsYaz0.Marshalling;
+using System.Runtime.InteropServices;
+
+namespace CsYaz0;
+
+internal unsafe static partial class Unmanaged
+{
+    private const string LibImport = "Lib/Yaz0";
+
+    [LibraryImport(LibImport)]
+    public static partial DataMarshal Compress(byte* src, int src_len, uint alignment, int level);
+
+    [LibraryImport(LibImport)]
+    public static partial void FreeVector(nint handle);
+
+    [LibraryImport(LibImport)]
+    public static partial void ExportVector(DataMarshal handle, out byte* dst, out int dst_len);
+}
