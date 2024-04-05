defc71dd8fb0c6e2126fca343e2880cac33d74fc
ArchLeaders <archleadership28@gmail.com>
2024-04-04 23:19:56 -0700

    Code cleanup (rename const)

--- src/CsYaz0/Unmanaged.cs
+++ src/CsYaz0/Unmanaged.cs
@@ -5,14 +5,14 @@ namespace CsYaz0;
 
 internal unsafe static partial class Unmanaged
 {
-    private const string LibImport = "Yaz0";
+    private const string LIB_IMPORT = "Yaz0";
 
-    [LibraryImport(LibImport)]
+    [LibraryImport(LIB_IMPORT)]
     public static partial DataMarshal Compress(byte* src, int src_len, uint alignment, int level);
 
-    [LibraryImport(LibImport)]
+    [LibraryImport(LIB_IMPORT)]
     public static partial void FreeVector(nint handle);
 
-    [LibraryImport(LibImport)]
+    [LibraryImport(LIB_IMPORT)]
     public static partial void ExportVector(DataMarshal handle, out byte* dst, out int dst_len);
 }
